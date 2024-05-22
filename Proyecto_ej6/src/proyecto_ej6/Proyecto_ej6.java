606cf5d06896c8bf7ac1472c6f6a3b06ec023b87
Jaled <116159488+Jaled3@users.noreply.github.com>
2024-05-22 11:50:57 +0200

    cambios

--- Proyecto_ej6/src/proyecto_ej6/Proyecto_ej6.java
+++ Proyecto_ej6/src/proyecto_ej6/Proyecto_ej6.java
@@ -15,7 +15,7 @@ public class Proyecto_ej6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Estos son algunos de los cambios hechos en local");
     }
     
 }
