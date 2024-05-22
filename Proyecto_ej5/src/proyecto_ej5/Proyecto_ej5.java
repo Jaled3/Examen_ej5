de7415ee0d56dad2c6f20b82c40d7dd31d0f44af
Jaled <116159488+Jaled3@users.noreply.github.com>
2024-05-22 11:38:33 +0200

    Lineas
    añadir líneas de código

--- Proyecto_ej5/src/proyecto_ej5/Proyecto_ej5.java
+++ Proyecto_ej5/src/proyecto_ej5/Proyecto_ej5.java
@@ -15,7 +15,8 @@ public class Proyecto_ej5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Estas son alguna lineas de codigo");
+        System.out.println("Y esta otra");
     }
     
 }
